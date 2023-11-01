fe699cede8a813b6ca2aa55ba2fa1bd6cf4faed8
Yasir Iqbal <105126069+RoxyBlockChain@users.noreply.github.com>
2023-11-01 16:46:29 +0500

    1st init

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import { useMoralis, useWeb3Contract } from "react-moralis"
+import { abi } from "../contracts/abi"
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  const { enableWeb3, isWeb3Enabled } = useMoralis()
+
+  const { runContractFunction, isFetching, isLoading } = useWeb3Contract({
+    abi : abi,
+    contractAddress: "0xd9145CCE52D386f254917e481eB44e9943F39138",
+    functionName: "store",
+    params:{
+      _favoriteNumber: 42,
+    }
+  })
+  
+  return (
+    <div>{ 
+      isWeb3Enabled ? (
+        <>
+          "Connected"
+          <button onClick={() => runContractFunction()}>Execute</button>
+        </>
+          ) : (
+          <button onClick={() => enableWeb3()}>Connect</button>
+          ) 
+          }
+    </div>   
+  );
+}
